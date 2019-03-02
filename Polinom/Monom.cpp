a1fcd5617883abbc140802e4398d5870e0287ffe
Михаил Лугин <329898@mail.ru>
2019-03-02 20:47:35 +0300

    Создана База.

---
+++ Polinom/Monom.cpp
@@ -0,0 +1,79 @@
+#include "Monom.h"
+
+Monom::Monom(double k, int x, int y, int z)
+{
+
+}
+
+Monom::~Monom()
+{
+
+}
+
+void Monom::SetXpower(int x)
+{
+
+}
+void Monom::SetYpower(int y)
+{
+
+}
+void Monom::SetZpower(int z)
+{
+
+}
+
+int Monom::GetXpower()
+{
+
+}
+
+int Monom::GetYpower()
+{
+
+}
+
+int Monom::GetZpower()
+{
+
+}
+
+void Monom::SetK(double k)
+{
+
+}
+
+double Monom::GetK()
+{
+
+}
+
+void Monom::SetNum(int N)
+{
+
+}
+
+int Monom::GetNum()
+{
+
+}
+
+bool Monom::operator==(const Monom &M) // сравнивает только показатели степени
+{
+
+}
+
+bool Monom::operator!=(const Monom &M)
+{
+
+}
+
+Monom& Monom::operator=(const Monom &M)
+{
+
+}
+
+Monom Monom::operator+(const Monom &M) // складывает только коэффиценты
+{
+
+}
---
+++ Polinom/Polinom.cpp
@@ -0,0 +1,86 @@
+#include"Polinom.h"
+
+Polinom::Polinom(std::string s)
+{
+
+}
+
+Polinom::Polinom(std::list<Monom> P1)
+{
+
+}
+
+Polinom::Polinom(Monom M)
+{
+
+}
+
+Polinom::~Polinom()
+{
+
+}
+
+void Polinom::recount() // перезадает номер мономам после удаления/добавления новых мономов, объединяет мономы с одинаковым p !после сортировки!
+{
+
+}
+
+void Polinom::sort() // сортирует мономы по переменной p (показатели степени)
+{
+
+}
+
+void Polinom::parsing() // жопоболь
+{
+
+}
+
+std::string Polinom::GetName()
+{
+
+}
+
+Polinom& Polinom::operator=(const Polinom &P)
+{
+
+}
+
+Polinom Polinom::operator+(const Polinom &P)
+{
+
+}
+
+Polinom Polinom::operator-(const Polinom &P)
+{
+
+}
+
+Polinom Polinom::operator*(const Polinom &P)
+{
+
+}
+
+Polinom Polinom::operator/(const Polinom &P)
+{
+
+}
+
+Polinom Polinom::operator%(const Polinom &P)
+{
+
+}
+
+Polinom Polinom::dx()
+{
+
+}
+
+Polinom Polinom::dy()
+{
+
+}
+
+Polinom Polinom::dz()
+{
+
+}
