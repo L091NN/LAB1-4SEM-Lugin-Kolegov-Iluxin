0fdf90be703b1c896e2f0d39a86ea9a9a072fcd9
Михаил Лугин <329898@mail.ru>
2019-03-31 16:38:06 +0300

    Complete!

--- Source/Table/main.cpp
+++ Source/Table/main.cpp
@@ -13,6 +13,19 @@ void Insert();
 void Find();
 void All();
 void Delete();
+void plus();
+void minus();
+void mult();
+void div();
+void mod();
+void dx();
+void dy();
+void dz();
+void Ix();
+void Iy();
+void Iz();
+void count();
+
 
 int main(int argc, char** argv) 
 {
@@ -37,6 +50,54 @@ int main(int argc, char** argv)
 		{
 			Delete();
 		}
+		if (command == "plus")
+		{
+			plus();
+		}
+		if (command == "minus")
+		{
+			minus();
+		}
+		if (command == "mult")
+		{
+			mult();
+		}
+		if (command == "div")
+		{
+			div();
+		}
+		if (command == "mod")
+		{
+			mod();
+		}
+		if (command == "dx")
+		{
+			dx();
+		}
+		if (command == "dy")
+		{
+			dy();
+		}
+		if (command == "dz")
+		{
+			dz();
+		}
+		if (command == "Ix")
+		{
+			Ix();
+		}
+		if (command == "Iy")
+		{
+			Iy();
+		}
+		if (command == "Iz")
+		{
+			Iz();
+		}
+		if (command == "count")
+		{
+			count();
+		}
 
 		std::cout << "USER>";
 		std::getline(std::cin, command);
@@ -54,6 +115,8 @@ void Insert()
 	std::cout << "Polynom: \n";
 	std::getline(std::cin, pol);
 
+	Polinom P(pol);
+	pol = P.GetPolinom_str();
 	TableRecord<std::string, std::string> tr;
 	tr.key = name;
 	tr.data = pol;
@@ -198,4 +261,638 @@ void Delete()
 	{
 		htl.Delete(key);
 	}
+}
+
+void plus()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom 1: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Polynom 2: \n";
+	std::getline(std::cin, pol);
+	Polinom P2(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P3((P1 + P2).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P3.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void minus()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom 1: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Polynom 2: \n";
+	std::getline(std::cin, pol);
+	Polinom P2(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P3((P1 - P2).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P3.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void mult()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom 1: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Polynom 2: \n";
+	std::getline(std::cin, pol);
+	Polinom P2(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P3((P1 * P2).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P3.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void div() // only on x
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom 1: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Polynom 2: \n";
+	std::getline(std::cin, pol);
+	Polinom P2(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P3((P1 / P2).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P3.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void mod() // only on x
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom 1: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Polynom 2: \n";
+	std::getline(std::cin, pol);
+	Polinom P2(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P3((P1 % P2).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P3.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void dx()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P2(P1.dx().GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P2.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void dy()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P2(P1.dy().GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P2.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void dz()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	Polinom P2(P1.dz().GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P2.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void Ix()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+	double C;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	std::cout << "value of const: \n";
+	std::getline(std::cin, name);
+	C = std::stod(name);
+
+	Polinom P2(P1.Ix(C).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P2.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void Iy()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+	double C;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	std::cout << "value of const: \n";
+	std::getline(std::cin, name);
+	C = std::stod(name);
+
+	Polinom P2(P1.Iy(C).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P2.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void Iz()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+	double C;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "Name of new polinom: \n";
+	std::getline(std::cin, name);
+
+	std::cout << "value of const: \n";
+	std::getline(std::cin, name);
+	C = std::stod(name);
+
+	Polinom P2(P1.Iz(C).GetPolinom_str(), name);
+
+	std::cout << "Table name: ";
+	std::getline(std::cin, table);
+
+	pol = P2.GetPolinom_str();
+	TableRecord<std::string, std::string> tr;
+	tr.key = name;
+	tr.data = pol;
+
+	if (table == "at")
+	{
+		at.Insert(tr);
+	}
+	if (table == "st")
+	{
+		st.Insert(tr);
+	}
+	if (table == "lt")
+	{
+		lt.Insert(tr);
+	}
+	if (table == "ht")
+	{
+		ht.Insert(tr);
+	}
+	if (table == "tt")
+	{
+		tt.Insert(tr);
+	}
+	if (table == "htl")
+	{
+		htl.Insert(tr);
+	}
+
+}
+
+void count()
+{
+	std::string name;
+	std::string pol;
+	std::string table;
+	double X;
+	double Y;
+	double Z;
+
+	std::cout << "Polynom: \n";
+	std::getline(std::cin, pol);
+	Polinom P1(pol);
+	pol = "";
+
+	std::cout << "value of x: \n";
+	std::getline(std::cin, name);
+	X = std::stod(name);
+	name = "";
+
+	std::cout << "value of y: \n";
+	std::getline(std::cin, name);
+	Y = std::stod(name);
+	name = "";
+
+	std::cout << "value of z: \n";
+	std::getline(std::cin, name);
+	Z = std::stod(name);
+	name = "";
+
+	std::cout << "Count in (" << X << ',' << Y << ',' << Z << ") equals: " << std::to_string(P1.count(X, Y, Z)) << std::endl;
+
 }
